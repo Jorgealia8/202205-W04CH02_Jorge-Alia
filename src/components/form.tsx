0b973346a693e3dce7c7a83c08ce509af21e0975
Jorge Alia <jorgealia94@gmail.com>
2022-05-31 19:59:13 +0200

    Complete first form and checked in console

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,29 +1,112 @@
-import React from 'react';
+import React, { useState } from 'react';
 export function FormFirst() {
+  const [formData, setFormData] = useState({
+    name: '',
+    lastname: '',
+    birthdate: '',
+    gender: '',
+    email: '',
+    information: Boolean,
+  });
+
+  function handleSubmit(ev: any) {
+    ev.preventDefault();
+    console.log('Guardando', formData);
+  }
+
+  function handleChange(ev: any) {
+    const value =
+      ev.target.type === 'checkbox' ? ev.target.checked : ev.target.value;
+    setFormData({ ...formData, [ev.target.name]: value });
+  }
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          <input type="text" name="name" placeholder="Name" />
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div>
-          <input type="text" name="last name" placeholder="Last name" />
+          <input
+            type="text"
+            name="lastname"
+            placeholder="Last name"
+            value={formData.lastname}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div>
-          <input type="text" name="Birthdate" placeholder="Birthdate" />
+          <input
+            type="date"
+            name="birthdate"
+            placeholder="Birthdate"
+            // checked={formData.birthdate}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div>
-          <input type="radio" name="Gender" id="male" />
+          <input
+            type="radio"
+            name="gender"
+            id="male"
+            value="male"
+            onChange={handleChange}
+            required
+          />
           <label htmlFor="Male">Male</label>
-          <input type="radio" name="Gender" id="female" />
+          <input
+            type="radio"
+            name="Gender"
+            id="female"
+            value="female"
+            onChange={handleChange}
+          />
           <label htmlFor="Female">Female</label>
-          <input type="radio" name="Gender" id="Other" />
+          <input
+            type="radio"
+            name="Gender"
+            id="Other"
+            value="Other"
+            onChange={handleChange}
+          />
           <label htmlFor="Other">Other</label>
-          <input type="radio" name="Gender" id="Prefernot" />
+          <input
+            type="radio"
+            name="gender"
+            id="Prefernot"
+            value="Prefernot"
+            onChange={handleChange}
+          />
           <label htmlFor="Prefer not to mention">Prefer not to mention</label>
         </div>
         <div>
-          <input type="checkbox" name="information" id="information" />
+          <div>
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <input
+            type="checkbox"
+            name="information"
+            id="information"
+            // checked={formData.information}
+            onChange={handleChange}
+          />
           <label htmlFor="information">
             ¿Do you want to receive information from our newsletter?
           </label>
